1e8820857e338326e6a87be8c63ea47bf07a3021
Glen Alan MacLachlan <maclach@gwu.edu>
2014-12-12 07:15:29 -0500

    Fixed submit.sh

--- SLURM/SingleThreadSRun/R_Example/submit.sh
+++ SLURM/SingleThreadSRun/R_Example/submit.sh
@@ -9,6 +9,5 @@
 #SBATCH	 -N 4
 #SBATCH  -n 64
 
-cd /home/maclach/Codes/HPC_Examples/SLURM/SingleThreadSRun/R_Exampl 
-#cd /home/maclach/Codes/HPC_Examples/SLURM/SingleThreadSRun/R_Example 
+cd /home/maclach/Codes/HPC_Examples/SLURM/SingleThreadSRun/R_Example 
 srun sh wrapper.sh
